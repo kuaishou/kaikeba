36be362b00a5b90345a7a2767b8262c2f4d146f5
kuaishou <947634673@qq.com>
2021-04-30 17:24:15 +0800

    'es6高阶手写promise和mvvm框架'

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var stack = [];
-stack.push(1);
-stack.push(2);
-const item1=stack.pop()
-const item2=stack.pop()
+import obj from "./inff/a";
+
+console.log(null==undefined)   //true
+console.log(null==false)   //false
+console.log(null==0)   //false
+console.log(false==0)   //true
+let num;
+console.log(num + 1)   //NaN
+
+function fun(obj ? ...Object.assign({a:123,b:456},obj) : ...{a:1,b:2}){
+    return {a,b}
+}
+fun()
